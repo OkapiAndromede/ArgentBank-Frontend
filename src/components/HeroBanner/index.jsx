b159a55dc0215aa21a44d75cfd588a6f126796ae
Neal Muller <neal.henri.muller@gmail.com>
2025-09-24 13:45:41 +0200

    feat (HeroBanner) : add backbones of HeroBanner component

---
+++ src/components/HeroBanner/index.jsx
@@ -0,0 +1,16 @@
+function HeroBanner() {
+  return (
+    <div className="heroBanner">
+      <section className="heroBanner__content">
+        <p className="heroBanner__content--subtitle">No fees.</p>
+        <p className="heroBanner__content--subtitle">No minimum deposit</p>
+        <p className="heroBanner__content--subtitle">High interest rates.</p>
+        <p className="heroBanner__content--text">
+          Open a savings account with Argent Bank today !
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default HeroBanner;
