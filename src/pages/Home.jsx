8f7a8264cefe615b5335c3f44c6c0094cdb45569
Neal Muller <neal.henri.muller@gmail.com>
2025-09-25 14:01:53 +0200

    feat : add Feature component with fecthData function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
+import Feature from "../components/Feature";
 import HeroBanner from "../components/HeroBanner";
 import Navigation from "../components/Navigation";
 
 function Home() {
+  const [features, setFeatures] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("/features.json");
+      const data = await response.json();
+      setFeatures(data?.features);
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Navigation />
       <HeroBanner />
-      Bonjour les gens
+      <section className="features">
+        {features?.map((item) => (
+          <Feature
+            key={item.id}
+            imageSrc={item.image}
+            imageAlt={item.description}
+            title={item.title}
+            text={item.content}
+          />
+        ))}
+      </section>
     </>
   );
 }
