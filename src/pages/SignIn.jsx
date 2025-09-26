5a8c6f41ff727098ab1b9b3882d05da2d0e0df35
Neal Muller <neal.henri.muller@gmail.com>
2025-09-26 14:33:36 +0200

    feat : add the basic structure of Sign In page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,16 @@
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+import Form from "../containers/Form";
 function SignIn() {
-  return "Page de connexion";
+  return (
+    <>
+      <Navigation />
+      <main className="mainSignIn">
+        <Form />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default SignIn;
