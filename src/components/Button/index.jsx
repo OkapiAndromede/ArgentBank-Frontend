728f561975dd6dbbf68dbd4a203d080a5b7bf6c7
Neal Muller <neal.henri.muller@gmail.com>
2025-10-01 16:02:55 +0200

    feat (minor) : add new buttonType prop

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,10 +3,10 @@ const BUTTON_TYPES = {
   DEFAULT: "default",
   SUBMIT: "submit",
 };
-function Button({ buttonType, children, isDisabled = false }) {
+function Button({ buttonType, children, isDisabled = false, buttonStyle }) {
   switch (buttonType) {
     case BUTTON_TYPES.DEFAULT:
-      return <button className="button"> {children} </button>;
+      return <button className={buttonStyle}> {children} </button>;
     case BUTTON_TYPES.SUBMIT:
       return (
         <input
