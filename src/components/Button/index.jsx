fd1ddfc5d3ad54c850a1bdf7ff323b5134146652
Neal Muller <neal.henri.muller@gmail.com>
2025-10-01 15:46:30 +0200

    refactor : add two type of button, default and submit

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,23 @@
 import "./style.scss";
-function Button({ children, isDisabled = false }) {
-  return (
-    <>
-      <input
-        className="button"
-        type="submit"
-        value={children}
-        {...(isDisabled && { disabled: true })}
-      />
-    </>
-  );
+const BUTTON_TYPES = {
+  DEFAULT: "default",
+  SUBMIT: "submit",
+};
+function Button({ buttonType, children, isDisabled = false }) {
+  switch (buttonType) {
+    case BUTTON_TYPES.DEFAULT:
+      return <button className="button"> {children} </button>;
+    case BUTTON_TYPES.SUBMIT:
+      return (
+        <input
+          className="button"
+          type="submit"
+          value={children}
+          {...(isDisabled && { disabled: true })}
+        />
+      );
+    default:
+      return <button value={children} className="defaultButton" />;
+  }
 }
 export default Button;
