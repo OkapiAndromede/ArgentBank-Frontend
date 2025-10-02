6eeae79d208d8b2fedd9bfbfd64a81306524aff1
Neal Muller <neal.henri.muller@gmail.com>
2025-10-02 11:23:08 +0200

    feat (architecture) : add Account and Footer component

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,12 +1,28 @@
+import Account from "../components/Account";
+import Button from "../components/Button";
 import Navigation from "../components/Navigation";
 import UserBanner from "../components/UserBanner";
+import Footer from "../components/Footer";
 function UserAccount() {
   return (
     <>
       <Navigation />
       <main className="mainUser">
         <UserBanner />
+        <section className="account">
+          <Account
+            title="Argent Bank Checking (x8349)"
+            amount={`$${2_082.79}`}
+            description="Available Balance"
+          />
+          <div>
+            <Button buttonType={"default"} buttonStyle="account__cta">
+              View transactions
+            </Button>
+          </div>
+        </section>
       </main>
+      <Footer />
     </>
   );
 }
