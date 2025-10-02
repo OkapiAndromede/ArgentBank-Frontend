a8da143c80ef622fc52904f54f37e62442799d9f
Neal Muller <neal.henri.muller@gmail.com>
2025-10-02 15:49:01 +0200

    refactoring (className) : optimization of className for style purpose

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -12,24 +12,24 @@ function Navigation({ isConnected = false, userName }) {
             alt="Argent Bank Logo"
           ></img>
         </NavLink>
-        <div className={"navBar__signOut"}>
+        <div className={"navBar__user"}>
           <svg
-            className="navBar__signOut--icon"
+            className="navBar__user--icon"
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 640 640"
           >
             <path d="M463 448.2C440.9 409.8 399.4 384 352 384L288 384C240.6 384 199.1 409.8 177 448.2C212.2 487.4 263.2 512 320 512C376.8 512 427.8 487.3 463 448.2zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM320 336C359.8 336 392 303.8 392 264C392 224.2 359.8 192 320 192C280.2 192 248 224.2 248 264C248 303.8 280.2 336 320 336z" />
           </svg>
-          <p className="navBar__signOut--text">{userName}</p>
-          <NavLink to={"/signIn"} className={"navBar__signOut--link"}>
+          <p className="navBar__user--text">{userName}</p>
+          <NavLink to={"/signIn"} className={"navBar__user--link"}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 640 640"
-              className="navBar__signOut--icon arrow"
+              className="navBar__user--icon arrow"
             >
               <path d="M569 337C578.4 327.6 578.4 312.4 569 303.1L425 159C418.1 152.1 407.8 150.1 398.8 153.8C389.8 157.5 384 166.3 384 176L384 256L272 256C245.5 256 224 277.5 224 304L224 336C224 362.5 245.5 384 272 384L384 384L384 464C384 473.7 389.8 482.5 398.8 486.2C407.8 489.9 418.1 487.9 425 481L569 337zM224 160C241.7 160 256 145.7 256 128C256 110.3 241.7 96 224 96L160 96C107 96 64 139 64 192L64 448C64 501 107 544 160 544L224 544C241.7 544 256 529.7 256 512C256 494.3 241.7 480 224 480L160 480C142.3 480 128 465.7 128 448L128 192C128 174.3 142.3 160 160 160L224 160z" />
             </svg>
-            <p className="navBar__signOut--text">Sign Out</p>
+            <p className="navBar__user--text">Sign Out</p>
           </NavLink>
         </div>
       </nav>
@@ -45,15 +45,15 @@ function Navigation({ isConnected = false, userName }) {
           ></img>
         </NavLink>
         <div>
-          <NavLink to={"/signIn"} className={"navBar__signIn"}>
+          <NavLink to={"/signIn"} className={"navBar__user"}>
             <svg
-              className="navBar__signIn--icon"
+              className="navBar__user--icon"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 640 640"
             >
               <path d="M463 448.2C440.9 409.8 399.4 384 352 384L288 384C240.6 384 199.1 409.8 177 448.2C212.2 487.4 263.2 512 320 512C376.8 512 427.8 487.3 463 448.2zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM320 336C359.8 336 392 303.8 392 264C392 224.2 359.8 192 320 192C280.2 192 248 224.2 248 264C248 303.8 280.2 336 320 336z" />
             </svg>
-            Sign In
+            <p className="navBar__user--text">Sign In</p>
           </NavLink>
         </div>
       </nav>
