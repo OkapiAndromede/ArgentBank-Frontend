f420ec580daabf167978f29120f625c0e1d21832
Neal Muller <neal.henri.muller@gmail.com>
2025-10-09 15:37:06 +0200

    feat : add slice for authentification with reducers and extraReducer

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { logIn } from "./authThunks";
+
+const initialState = {
+  token: null,
+  isAuthenticated: false,
+  status: "idle",
+  error: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    logOut: (state) => {
+      state.token = null;
+      state.isAuthenticated = false;
+      state.status = "idle";
+      state.error = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(logIn.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.token = action.payload.token;
+        state.isAuthenticated = true;
+      })
+      .addCase(logIn.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
+});
+
+export const { logOut } = authSlice.actions;
+export default authSlice.reducer;
