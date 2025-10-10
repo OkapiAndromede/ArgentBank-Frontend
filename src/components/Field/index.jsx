7209d601b0392ed2be24645247d217893a1906d8
Neal Muller <neal.henri.muller@gmail.com>
2025-10-10 10:32:21 +0200

    doc : add JSdoc for the component

--- src/components/Field/index.jsx
+++ src/components/Field/index.jsx
@@ -1,3 +1,16 @@
+/**
+ * Composant Field affichant les champs pour un formulaire
+ * Ce composant prends les props suivantes :
+ * @param {string} inputType - Le type d'input (ex : "text", "password", "checkbox")
+ * @param {string} inputId - L'identifiant unique de l'input
+ * @param {string} inputName - le nom de l'input
+ * @param {boolean} [isRequired = fasle]  - Détermine si le champ est requis
+ * @param {string} value - la valeur actuelle du champ
+ * @param {Function} onChange - fonction appellée lors d'une modification du champ
+ *
+ * @returns {JSX.Element} composant Field rendu
+ */
+
 function Field({
   inputType = "text",
   inputId,
