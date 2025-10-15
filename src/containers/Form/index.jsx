d6ac8900a7a224b5925b205133686a1286f24786
Neal Muller <neal.henri.muller@gmail.com>
2025-10-15 16:33:33 +0200

    refactoring : add form manager (react-hook-form)

--- src/containers/Form/index.jsx
+++ src/containers/Form/index.jsx
@@ -2,19 +2,25 @@ import Button from "../../components/Button";
 import Field from "../../components/Field";
 import "./style.scss";
 import useLoginForm from "../../hooks/useLoginForm";
+import { useForm } from "react-hook-form";
 /**
  * Composant Form affichant un formulaire de connexion utilisateur
  *
- * Ce composant utilise le hook personnalisé {@link useLoginForm} pour gérer :
- * - Les états internes du formulaire ("email", "password")
- * - La fonction handleSubmit pour la soumission du formulaire
+ * Ce composant gère la saisie et la validation des identifiants de connexion à l’aide de **React Hook Form**
+ * et du hook personnalisé {@link useLoginForm}, qui encapsule la logique d’authentification.
  * @component
  *
  * @returns {JSX.Element} composant Form rendu.
  */
 function Form() {
-  const { email, password, setEmail, setPassword, handleSubmit } =
-    useLoginForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const { handleLoginSubmit } = useLoginForm();
+  console.log("refresh");
   return (
     <section className="signIn">
       <svg
@@ -25,16 +31,16 @@ function Form() {
         <path d="M463 448.2C440.9 409.8 399.4 384 352 384L288 384C240.6 384 199.1 409.8 177 448.2C212.2 487.4 263.2 512 320 512C376.8 512 427.8 487.3 463 448.2zM64 320C64 178.6 178.6 64 320 64C461.4 64 576 178.6 576 320C576 461.4 461.4 576 320 576C178.6 576 64 461.4 64 320zM320 336C359.8 336 392 303.8 392 264C392 224.2 359.8 192 320 192C280.2 192 248 224.2 248 264C248 303.8 280.2 336 320 336z" />
       </svg>
       <h1>Sign In</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(handleLoginSubmit)}>
         <div className="inputWrapper">
           <label htmlFor="username">Username</label>
           <Field
             inputType="text"
             inputId="username"
             inputName="email"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
             isRequired={true}
+            register={register}
+            error={errors.email && "Email requis"}
           />
         </div>
         <div className="inputWrapper">
@@ -43,9 +49,9 @@ function Form() {
             inputType="password"
             inputId="password"
             inputName="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
             isRequired={true}
+            register={register}
+            error={errors.password && "Mot de passe requis"}
           />
         </div>
         <div className="inputRemember">
@@ -53,6 +59,7 @@ function Form() {
             inputType="checkbox"
             inputId="remember-me"
             inputName="remember-me"
+            register={register}
           />
           <label htmlFor="remember-me">Remember me</label>
         </div>
