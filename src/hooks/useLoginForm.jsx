02e341378f41fc64d20efb7eabe6c3ef749147df
Neal Muller <neal.henri.muller@gmail.com>
2025-10-15 16:35:12 +0200

    refactoring : delete state management due to form management by react hook form

--- src/hooks/useLoginForm.jsx
+++ src/hooks/useLoginForm.jsx
@@ -5,29 +5,18 @@ import { logIn } from "../features/auth/authThunks";
 /**
  * Hook personalisé gérant la logique du formulaire de connexion
  *
- * Le hook assure les tâches suivantes :
- * 1. La gestion des états internes du formulaire ("email", "password")
- * 2. La soumission du formulaire et la gestion du flux de connexion via Redux
+ * Le hook assure la logique d'authentification
  *
  * @function
- * @returns {object} Un objet contenant :
- * @returns {string} return.email - L'adresse mail saisie par l'utilisateur
- * @returns {string} return.password - Le mot de passe saisi par l'utilisateur
- * @returns {Function} return.setEmail - Le mutateur de "email"
- * @returns {Function} return.setPassword - Le mutateur de "password"
- * @returns {Function} return.handleSubmit - Gère la soumission du formulaire et la redirection après connexion
+ * @returns {Function} return.handleLoginSubmit - Gère l'authentification et la redirection après connexion
  */
 export default function useLoginForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  //Etats internes du formulaire
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   //Soumission du formulaire
-  async function handleSubmit(event) {
-    event.preventDefault();
+  async function handleLoginSubmit(formData) {
+    const { email, password } = formData;
 
     try {
       const resultAction = await dispatch(logIn({ email, password }));
@@ -45,10 +34,6 @@ export default function useLoginForm() {
 
   //le hook expose uniquement ce dont le composant a besoin
   return {
-    email,
-    password,
-    setEmail,
-    setPassword,
-    handleSubmit,
+    handleLoginSubmit,
   };
 }
