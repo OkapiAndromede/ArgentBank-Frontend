cfcf641b5ec7cbb7122c79f7363268870700f67c
Neal Muller <neal.henri.muller@gmail.com>
2025-10-17 17:08:25 +0200

    feat (BREAK) : add swicth case to manage rember me option

--- src/hooks/useLoginForm.jsx
+++ src/hooks/useLoginForm.jsx
@@ -16,16 +16,24 @@ export default function useLoginForm() {
 
   //Soumission du formulaire
   async function handleLoginSubmit(formData) {
-    const { email, password } = formData;
+    const { email, password, rememberMe } = formData;
 
     try {
       const resultAction = await dispatch(logIn({ email, password }));
-      if (logIn.fulfilled.match(resultAction)) {
-        //Connexion réussie
-        navigate("/userAccount");
-      } else {
-        //Connexion echoué
-        console.log("Erreur de connexion :", resultAction.payload);
+      switch (true) {
+        //Cas 1 : Connexion reussie
+        case logIn.fulfilled.match(resultAction) && !rememberMe:
+          navigate("/userAccount");
+          break;
+        //Cas 2 : Connexion reussie +rememberMe
+        case logIn.fulfilled.match(resultAction) && rememberMe:
+          localStorage.setItem("email", email);
+          localStorage.setItem("password", password);
+          navigate("/userAccount");
+          break;
+        default:
+          console.log("Erreur de connexion :", resultAction.payload);
+          break;
       }
     } catch (err) {
       console.error("Erreur inattendue:", err);
