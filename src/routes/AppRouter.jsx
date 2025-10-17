47e8549bbd9fe2ea86ec6dccc5c5d85fc404da2d
Neal Muller <neal.henri.muller@gmail.com>
2025-10-17 13:37:29 +0200

    feat : add ToastContainer to manage micro message with UI

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,16 +2,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import UserAccount from "../pages/UserAccount";
+import { ToastContainer, Flip } from "react-toastify";
 
 function AppRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/signIn" element={<SignIn />}></Route>
-        <Route path="/userAccount" element={<UserAccount />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Flip}
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/signIn" element={<SignIn />}></Route>
+          <Route path="/userAccount" element={<UserAccount />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
