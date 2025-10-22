093d7d408dcc8e6a9e92225f19b8395619d3f2de
Neal Muller <neal.henri.muller@gmail.com>
2025-10-22 17:30:19 +0200

    feat (h1 section) : add dynamic  render of userName

--- src/components/UserBanner/index.jsx
+++ src/components/UserBanner/index.jsx
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../Button";
 import "./style.scss";
+import { getUserData } from "../../features/user/userThunks";
+import { useEffect } from "react";
 function UserBanner() {
+  const dispatch = useDispatch();
+  const userName = useSelector((state) => state.user.userName);
+  const token = useSelector((state) => state.auth.token);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(getUserData());
+    }
+  }, [token]);
+
   return (
     <div className="headerUser">
       <h1>
-        Welcome back <br /> Tony Jarvis !
+        Welcome back <br /> {userName || "Tony Jarvis"} !
       </h1>
       <Button buttonStyle="editButton" buttonType="default">
         Edit Name
