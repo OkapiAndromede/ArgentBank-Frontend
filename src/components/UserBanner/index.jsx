e57dc050832522cd7ace320bd3444d084acb207b
Neal Muller <neal.henri.muller@gmail.com>
2025-10-23 14:15:50 +0200

    feat : add conditionnal rendering with isModalOpen

--- src/components/UserBanner/index.jsx
+++ src/components/UserBanner/index.jsx
@@ -2,11 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import Button from "../Button";
 import "./style.scss";
 import { getUserData } from "../../features/user/userThunks";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import EditNameModal from "../EditNameModal";
 function UserBanner() {
   const dispatch = useDispatch();
   const userName = useSelector((state) => state.user.userName);
   const token = useSelector((state) => state.auth.token);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     if (token) {
@@ -14,15 +16,23 @@ function UserBanner() {
     }
   }, [token]);
 
-  return (
-    <div className="headerUser">
-      <h1>
-        Welcome back <br /> {userName || "Tony Jarvis"} !
-      </h1>
-      <Button buttonStyle="editButton" buttonType="default">
-        Edit Name
-      </Button>
-    </div>
-  );
+  if (isModalOpen) {
+    return <EditNameModal onClose={() => setIsModalOpen(false)} />;
+  } else {
+    return (
+      <div className="headerUser">
+        <h1>
+          Welcome back <br /> {userName || "Tony Jarvis"} !
+        </h1>
+        <Button
+          buttonStyle="editButton"
+          buttonType="default"
+          onClick={() => setIsModalOpen(true)}
+        >
+          Edit Name
+        </Button>
+      </div>
+    );
+  }
 }
 export default UserBanner;
