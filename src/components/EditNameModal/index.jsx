4cf1d9829845e860fe89a3f2189898f77cff80e1
Neal Muller <neal.henri.muller@gmail.com>
2025-10-23 14:13:35 +0200

    feat : add v1 of modal editor without main fonctionnality

---
+++ src/components/EditNameModal/index.jsx
@@ -0,0 +1,59 @@
+import { useForm } from "react-hook-form";
+import Button from "../Button";
+import { useSelector } from "react-redux";
+
+function EditNameModal({ onClose }) {
+  const defaultFirstName = useSelector((state) => state.user.firstName);
+  const defaultLastName = useSelector((state) => state.user.lastName);
+  const {
+    register,
+    handleSubmit,
+    formState: { error },
+  } = useForm({
+    defaultValues: {
+      firstName: defaultFirstName,
+      lastName: defaultLastName,
+    },
+  });
+  function onSubmit(data) {
+    onClose();
+  }
+  return (
+    <section className="userModal">
+      <h1 className="userModal__title">Edit user info</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="editingInput">
+          <label htmlFor="username">User name :</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", {
+              required: "Le nouveau nom d'utilisateur est requis",
+            })}
+          />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="firstName">First name :</label>
+          <input
+            type="text"
+            id="firstName"
+            {...register("firstName")}
+            disabled
+          />
+        </div>
+        <div className="inputWrapper">
+          <label htmlFor="lastName">Last name :</label>
+          <input type="text" id="lastName" {...register("lastName")} disabled />
+        </div>
+        <div className="buttonChoice">
+          <Button buttonType="submit">Save</Button>
+          <Button buttonType="default" onClick={onClose}>
+            Cancel
+          </Button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default EditNameModal;
