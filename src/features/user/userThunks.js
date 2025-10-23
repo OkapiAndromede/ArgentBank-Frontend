a098e4ea6902a0bdbc3ef92024ba55da27e4dc9d
Neal Muller <neal.henri.muller@gmail.com>
2025-10-23 16:38:10 +0200

    feat : add putUserData API function

--- src/features/user/userThunks.js
+++ src/features/user/userThunks.js
@@ -20,3 +20,24 @@ export const getUserData = createAsyncThunk(
     }
   }
 );
+
+export const putUserData = createAsyncThunk(
+  "user/putUserData",
+  async ({ userName }, { getState, rejectWithValue }) => {
+    try {
+      const token = getState().auth.token;
+      const response = await axios.put(
+        "http://localhost:3010/api/v1/user/profile",
+        { userName },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "erreur serveur");
+    }
+  }
+);
