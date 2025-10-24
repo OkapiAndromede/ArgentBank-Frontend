863c66becd89eca503bd4e688d455866eba9f8a2
Neal Muller <neal.henri.muller@gmail.com>
2025-10-24 10:46:21 +0200

    doc (formatAmount) : add JSdoc for the formatAmount function

--- src/pages/utils.js
+++ src/pages/utils.js
@@ -1,3 +1,15 @@
+/**
+ * Formate les montants d'un tableau d'objet :
+ * - Convertit les montants au format local "en-US" (séparateur de millier, deux décimales)
+ * - Remplace les virgules par des espaces pour un affichage européen
+ *
+ * @param {object[]} table - Tableau d'objet contenant les données bancaires
+ * @returns {object[]} Nouveau tableau avec les montants formatés en chaîne de caractères
+ *
+ * @example
+ * formatAmount ([{amount : 1234.5}])
+ * tableau d'objet retourné -> [{amount : "1 234.50"}]
+ */
 export function formatAmount(table) {
   return table.map((item) => ({
     ...item,
