7c1961ea451e2b1583199d259ef3f5c8c0ffdc1b
Neal Muller <neal.henri.muller@gmail.com>
2025-10-24 19:58:20 +0200

    fix (BREAK) : securize the account access with localToken and isAuthenticated variable

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -3,12 +3,19 @@ import Button from "../components/Button";
 import Navigation from "../components/Navigation";
 import UserBanner from "../components/UserBanner";
 import Footer from "../components/Footer";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { formatAmount } from "./utils";
+import { useNavigate } from "react-router-dom";
+import { connectUser } from "../features/auth/authSlice";
 function UserAccount() {
   const userName = useSelector((state) => state.user.userName);
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+
+  const localToken = localStorage.getItem("token");
+  const navigate = useNavigate();
   const [funds, setFunds] = useState([]);
 
   useEffect(() => {
@@ -20,31 +27,42 @@ function UserAccount() {
     }
     fetchData();
   }, []);
-  console.log(funds);
-  return (
-    <>
-      <Navigation isConnected={true} userName={userName} />
-      <main className="mainUser">
-        <UserBanner />
-        {funds?.map((item) => (
-          <section className="account">
-            <Account
-              key={item.id}
-              title={item.title}
-              amount={`$${item.amount}`}
-              description={item.description}
-            />
-            <div>
-              <Button buttonType={"default"} buttonStyle="account__cta">
-                View transactions
-              </Button>
-            </div>
-          </section>
-        ))}
-      </main>
-      <Footer />
-    </>
-  );
+
+  useEffect(() => {
+    if (!localToken && !isAuthenticated) {
+      return navigate("/signIn");
+    } else {
+      dispatch(connectUser());
+    }
+  }, []);
+
+  //Renvoi l'utilisateur sur la page de connexion s'il n'est pas authentifié
+  if (isAuthenticated || localToken) {
+    return (
+      <>
+        <Navigation wantToConnect={false} userName={userName} />
+        <main className="mainUser">
+          <UserBanner />
+          {funds?.map((item) => (
+            <section className="account">
+              <Account
+                key={item.id}
+                title={item.title}
+                amount={`$${item.amount}`}
+                description={item.description}
+              />
+              <div>
+                <Button buttonType={"default"} buttonStyle="account__cta">
+                  View transactions
+                </Button>
+              </div>
+            </section>
+          ))}
+        </main>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default UserAccount;
