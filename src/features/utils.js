11f7381a28952d4a60b85df917a7d6341c81ff63
Neal Muller <neal.henri.muller@gmail.com>
2025-11-06 17:27:23 +0100

    feat (safeChangeItem) : add new function to updtate localStorage

--- src/features/utils.js
+++ src/features/utils.js
@@ -1,7 +1,30 @@
 export function safeGetItem(key) {
-  if (typeof localStorage === "undefined") return "";
-  if (typeof key === "string") return localStorage.getItem(key) || "";
-  if (typeof key === "boolean") return localStorage.getItem(key) || false;
-  //Par défaut
-  return "";
+  try {
+    const userString = localStorage.getItem("user");
+    if (!userString) return "";
+    const rememberedUser = JSON.parse(userString);
+    //Si la clé n'existe pas
+    if (!(key in rememberedUser)) return "";
+    return rememberedUser[key];
+  } catch (err) {
+    console.error("Erreur dans le safeGetItem", err);
+    //Par défaut
+    return "";
+  }
+}
+
+export function safeChangeItem(key, newValue) {
+  try {
+    const userString = localStorage.getItem("user");
+    if (!userString) return "";
+    const rememberedUser = JSON.parse(userString);
+    if (key in rememberedUser) {
+      //Mise à jour ou création de la clé
+      rememberedUser[key] = newValue;
+      //Reecriture dans le localStorage
+      localStorage.setItem("user", JSON.stringify(rememberedUser));
+    }
+  } catch (err) {
+    console.error("Erreur dans le safeChangeItem", err);
+  }
 }
