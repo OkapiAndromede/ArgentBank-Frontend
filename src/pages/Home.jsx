529ff3fdd19bb0ade1b0e1a86e8c923078ed90ad
Neal Muller <neal.henri.muller@gmail.com>
2025-11-06 17:28:52 +0100

    refactoring (BREAK) : add a autoLogIn function in case of rememberMe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,16 @@ import Feature from "../components/Feature";
 import HeroBanner from "../components/HeroBanner";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { connectUser } from "../features/auth/authSlice";
+import { logIn } from "../features/auth/authThunks";
 
 function Home() {
   const [features, setFeatures] = useState([]);
   const dispatch = useDispatch();
+  const userEmail = useSelector((state) => state.user.userEmail);
+  const userPassword = useSelector((state) => state.user.userPassword);
+  const rememberMe = useSelector((state) => state.auth.isRemember);
 
   useEffect(() => {
     async function fetchData() {
@@ -20,10 +24,23 @@ function Home() {
   }, []);
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      dispatch(connectUser());
-    }
+    const autoLogIn = async () => {
+      if (!rememberMe || !userEmail || !userPassword) return;
+
+      try {
+        const resultAction = await dispatch(
+          logIn({ email: userEmail, password: userPassword })
+        );
+        if (logIn.fulfilled.match(resultAction)) {
+          dispatch(connectUser());
+        } else {
+          console.warn("connexion automatique échouée", resultAction);
+        }
+      } catch (err) {
+        console.error("Erreur lors de la connexion automatique : ", err);
+      }
+    };
+    autoLogIn();
   }, [dispatch]);
 
   return (
