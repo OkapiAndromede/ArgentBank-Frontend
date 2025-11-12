289bbfa9e5f2037ec4fbe204bc0e61f2a2cac5af
Neal Muller <neal.henri.muller@gmail.com>
2025-11-12 18:40:54 +0100

    docs: add JSdoc for the component

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -1,4 +1,15 @@
 import "./style.scss";
+/**
+ * Composant Account affichant les informations générales d'un compte bancaire
+ * Ce composant présente le titre, le montant et une courte description du compte
+ *
+ * @component
+ * @param {string} title - Le titre du compte bancaire
+ * @param {number} amount - Le montant total du compte bancaire
+ * @param {string} description - La description du compte bancaire
+ *
+ * @returns {JSX.Element} composant Account rendu
+ */
 function Account({ title, amount, description }) {
   return (
     <div className="accountContent">
