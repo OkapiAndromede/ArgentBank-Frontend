614920f02ef3c8ab793d089cb7225772299a2272
Neal Muller <neal.henri.muller@gmail.com>
2025-11-12 19:03:43 +0100

    doc : add JSdoc for the component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,4 +1,20 @@
 import "./style.scss";
+/**
+ * Composant Button gérant l'affichage et le comportement des différents types de boutons
+ *
+ * Ce composant prend en charge deux types de boutons :
+ * - "default" : un bouton standard
+ * - "submit" : un bouton associé à la soumission d'un formulaire
+ *
+ * @component
+ * @param {"default" | "submit"} buttonType - Le type de bouton à afficher
+ * @param {React.ReactNode} children - Le contenu textuel ou JSX à afficher dans le bouton
+ * @param {boolean} [isDisabled = false] - Indique si le bouton est désactivé
+ * @param {string} [buttonStyle] - La class CSS appliquée au bouton
+ * @param {Function} onClick - Fonction appelée lors du clic sur le bouton (uniquement pour le type `default`)
+ *
+ * @returns {JSX.Element} composant Button rendu
+ */
 const BUTTON_TYPES = {
   DEFAULT: "default",
   SUBMIT: "submit",
