dbe5dcb0139c8b7893633231d2369faf3ac5cfe0
Neal Muller <neal.henri.muller@gmail.com>
2025-11-12 12:09:38 +0100

    feat (ToastContainer): change time to autoClose property

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -9,7 +9,7 @@ function AppRouter() {
     <>
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick={false}
