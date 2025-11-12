eb3975a018993c59e405579da61cff00a5279904
Neal Muller <neal.henri.muller@gmail.com>
2025-11-12 17:31:49 +0100

    feat (html element): add h2 title

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -3,6 +3,7 @@ function HeroBanner() {
   return (
     <div className="heroBanner">
       <section className="heroBanner__content">
+        <h2 className="srOnly">Promoted Content</h2>
         <p className="heroBanner__content--subtitle">No fees.</p>
         <p className="heroBanner__content--subtitle">No minimum deposit</p>
         <p className="heroBanner__content--subtitle">High interest rates.</p>
