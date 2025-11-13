ae9e82d8ee099f3df3709cf1324dd520791c7776
Neal Muller <neal.henri.muller@gmail.com>
2025-11-13 11:48:33 +0100

    doc : add JSdoc for the component

--- src/components/Feature/index.jsx
+++ src/components/Feature/index.jsx
@@ -1,4 +1,17 @@
 import "./style.scss";
+/**
+ * Composant Feature affichant une fonctionnalité avec son icône, son titre et sa description
+ *
+ * Utilisé pour présenter une caractéristique d'un produit
+ *
+ * @component
+ * @param {string} imageSrc - Chemin absolu vers la ressource de l'image
+ * @param {string} imageAlt - Description alternative de l'image
+ * @param {string} title - Titre de la fonctionalité
+ * @param {string} text - Explication de la fonctionalité
+ *
+ * @returns {JSX.Element} composant Feature rendu
+ */
 function Feature({ imageSrc, imageAlt, title, text }) {
   return (
     <article className="featureItem">
