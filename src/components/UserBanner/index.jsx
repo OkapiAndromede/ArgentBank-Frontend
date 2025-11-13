49740034e3aee0f3c164cb675ef2dd5ec17d0660
Neal Muller <neal.henri.muller@gmail.com>
2025-11-13 13:29:20 +0100

    doc : add JSdoc for UserBanner component

--- src/components/UserBanner/index.jsx
+++ src/components/UserBanner/index.jsx
@@ -2,6 +2,19 @@ import Button from "../Button";
 import "./style.scss";
 import { useState } from "react";
 import EditNameModal from "../EditNameModal";
+/**
+ * Composant UserBanner affichant la bannière d'accueil utilisateur.
+ *
+ * Ce composant affiche soit :
+ * - Une bannière de bienvenue contenant le nom d'utilisateur et un bouton pour modifier le nom
+ * - La modale {@link EditNameModal} lorsque l'utilisateur souhaite éditer le userName
+ *
+ * @component
+ * @param {string} userName - Le nom d'utilisateur affiché dans la bannière
+ *
+ * @returns {JSX.Element} composant UserBanner rendu
+ */
+
 function UserBanner({ userName }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
