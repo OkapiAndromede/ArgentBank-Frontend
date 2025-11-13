435668d4471afb5f1d68b2775a3e0f6d8c9ed2ff
Neal Muller <neal.henri.muller@gmail.com>
2025-11-13 21:28:47 +0100

    doc : add JSdoc for function

--- src/features/utils.js
+++ src/features/utils.js
@@ -1,3 +1,14 @@
+/**
+ * Fonction qui récupère une valeur spécifique depuis le `localStorage`.
+ *
+ * En particulier, elle lit la clé "user" dans le `localStorage`, tente de la parser en JSON,
+ * puis retourne la valeur correspondante à la propriété demandée (`key`).
+ * Si la clé n'existe pas, si le `localStorage` est vide ou si une erreur survient,
+ * la fonction renvoie une chaîne vide.
+ *
+ * @param {string} key - Nom de la propriété à récupérer dans l'objet `user` du `localStorage`.
+ * @returns {string} La valeur associée à la clé demandée,ou une chaîne vide en cas d'erreur ou de valeur inexistante.
+ */
 export function safeGetItem(key) {
   try {
     const userString = localStorage.getItem("user");
@@ -13,6 +24,17 @@ export function safeGetItem(key) {
   }
 }
 
+/**
+ * Fonction qui change la valeur d'une propriété spécifique depuis le `localStorage`.
+ *
+ * En particulier, elle lit la clé "user" dans le `localStorage`, tente de la parser en JSON,
+ * puis met à jour ou creé la clé. Pour finir, elle string la clé `"user"`.
+ * Si le `localStorage` est vide ou si une erreur survient,
+ * la fonction renvoie une chaîne vide.
+ * @param {string} key - Nom de la propriété à récupérer dans l'objet `user` du `localStorage`.
+ * @param {string} newValue - Nouvelle valeur à associé à la propriété.
+ * @returns {void} Cette fonction ne renvoie rien.
+ */
 export function safeChangeItem(key, newValue) {
   try {
     const userString = localStorage.getItem("user");
